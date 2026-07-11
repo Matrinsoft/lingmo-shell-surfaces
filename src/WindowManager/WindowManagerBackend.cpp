7825d6a8c0432caecbe674b5beec1f365e11f0f7
Lingmo <lingmo@lingmo.org>
2026-07-11 23:23:06 +0800

    #2fix(ci)

--- src/WindowManager/WindowManagerBackend.cpp
+++ src/WindowManager/WindowManagerBackend.cpp
@@ -1,6 +1,12 @@
 #include "WindowManagerBackend.h"
 #include "KWinBackend.h"
 
+#ifdef LINGMO_HAVE_X11
+#include "X11/X11WindowBackend.h"
+#endif
+
+#include <QGuiApplication>
+
 namespace Lingmo {
 
 WindowManagerBackend::WindowManagerBackend(QObject *parent)
@@ -12,6 +18,15 @@ WindowManagerBackend::~WindowManagerBackend() = default;
 
 WindowManagerBackend *WindowManagerBackend::create(QObject *parent)
 {
+    const QString platform = QGuiApplication::platformName();
+
+#ifdef LINGMO_HAVE_X11
+    if (platform == QLatin1String("xcb")) {
+        return new X11WindowBackend(nullptr, parent);
+    }
+#endif
+
+    // Wayland (KWin): use KWin D-Bus + zwlr_foreign_toplevel
     return new KWinBackend(parent);
 }
 
